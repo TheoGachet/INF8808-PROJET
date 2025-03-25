942a787699a92ab499d1849ff6d5f096a12becf8
Calou_Tech <luxfer92@gmail.com>
2025-03-25 10:40:53 -0400

    coucou

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,10 +1,6 @@
 import pandas as pd
-<<<<<<< HEAD
 import numpy as np
+import matplotlib.pyplot as plt
 
-import matplotlib.pyplot as plt
-=======
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
->>>>>>> 41ebbea50ee880be6cd61e19f661fde0092c4110
+#Test coucou
+
