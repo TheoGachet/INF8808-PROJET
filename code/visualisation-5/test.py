c105600672517f75417e9e24bc817e88c6fbafbe
Matthieu Speismann <matt.speismann@gmail.com>
2025-03-25 10:42:30 -0400

    Viz 5 (+ test github perso)

---
+++ code/visualisation-5/test.py
@@ -0,0 +1,12 @@
+# j'ai utilisé le TP3 comme base pour ajouter une visualisation dessusc
+
+import preprocess as preprocess
+
+if __name__ == "__main__":
+    try:
+        df = preprocess.load_csv("INF8808/INF8808-PROJET/data/all_athlete_games.csv")
+        print("CSV chargé avec succès")
+        print(df.head())
+    except Exception as e:
+        print("Erreur lors du chargement du CSV")
+        print(e)
